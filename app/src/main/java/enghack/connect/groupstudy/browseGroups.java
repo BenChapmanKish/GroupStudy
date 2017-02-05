7ed4b9c29f400f3cbadd85ca9f9a94116ee53538
Haoxi (Steve) Huang <haoxi98@gmail.com>
2017-02-04 19:45:34 -0500

    Updated with new activities on button push.

---
+++ app/src/main/java/enghack/connect/groupstudy/browseGroups.java
@@ -0,0 +1,16 @@
+package enghack.connect.groupstudy;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class browseGroups extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_groups);
+
+    }
+}
---
+++ app/src/main/java/enghack/connect/groupstudy/findGroups.java
@@ -0,0 +1,15 @@
+package enghack.connect.groupstudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class findGroups extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_groups);
+
+
+    }
+}
