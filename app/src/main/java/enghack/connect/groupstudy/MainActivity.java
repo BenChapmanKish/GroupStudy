a5d7a3a0d4042fd2066bfd38df7aba314103266d
Haoxi (Steve) Huang <haoxi98@gmail.com>
2017-02-05 06:15:36 -0500

    List View

--- app/src/main/java/enghack/connect/groupstudy/MainActivity.java
+++ app/src/main/java/enghack/connect/groupstudy/MainActivity.java
@@ -3,8 +3,13 @@ package enghack.connect.groupstudy;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
@@ -13,6 +18,9 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
 	private static final String TAG = "MainActivity: ";
@@ -20,11 +28,33 @@ public class MainActivity extends AppCompatActivity {
 	private DatabaseReference mDatabase;
 	FirebaseDatabase database;
 
+	private RecyclerView mRecyclerView;
+	private RecyclerView.Adapter mAdapter;
+	private RecyclerView.LayoutManager mLayoutManager;
+	private List<Lists> myDataset = new ArrayList<>();
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+		// use this setting to improve performance if you know that changes
+		// in content do not change the layout size of the RecyclerView
+		mRecyclerView.setHasFixedSize(true);
+
+		// use a linear layout manager
+		mLayoutManager = new LinearLayoutManager(this);
+		mRecyclerView.setLayoutManager(mLayoutManager);
+
+		// specify an adapter (see also next example)
+		mAdapter = new ListAdapter(myDataset);
+		mRecyclerView.setAdapter(mAdapter);
+
+		Lists list = new Lists("Group Yay");
+		myDataset.add(list);
 
 		// Initialize the facebook logging and analytics
 		FacebookSdk.sdkInitialize(getApplicationContext());
